2a6ec506f1b4096f8b081b8a4268c74dc7c00956
bhoomip16 <bhoomistudy16@gmail.com>
2026-03-02 23:43:59 +0530

    UC10: Case Sensitive

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,29 @@
 import java.util.*;
 
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class PalindromeCheckerApp {
 
-    static Node createList(String str) {
-        Node head = null, tail = null;
+    static boolean palidrome(String s , int start , int end){
+        if(start >= end) return  true;
+        if(s.charAt(start)!=s.charAt(end)) return false;
+        return palidrome(s,start+1,end -1);
+        public static boolean isPalindrome(String input) {
 
-        for (char ch : str.toCharArray()) {
-            Node newNode = new Node(ch);
+            String normalized = input.toLowerCase().replaceAll("\\s+", "");
 
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
+            char[] charArray = normalized.toCharArray();
+
+            int left = 0;
+            int right = charArray.length - 1;
+
+            while (left < right) {
+                if (charArray[left] != charArray[right]) {
+                    return false;
+                }
+                left++;
+                right--;
             }
-        }
-        return head;
-    }
 
-    static boolean isPalindrome(Node head) {
-        if (head == null || head.next == null)
             return true;
-
-        Node slow = head;
-        Node fast = head;
-
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data)
-                return false;
-
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
-
-    static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-        return prev;
-        static boolean palidrome(String s , int start , int end){
-            if(start >= end) return  true;
-            if(s.charAt(start)!=s.charAt(end)) return false;
-            return palidrome(s,start+1,end -1);
         }
 
         public static void main(String[] args) {
@@ -79,10 +31,15 @@ public class PalindromeCheckerApp {
             System.out.print("Input text : ");
             String input = sc.nextLine();
 
-            Node head = createList(input);
-            System.out.println("Is Palindrom? :"+isPalindrome(head));
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter a string: ");
+            String input = scanner.nextLine();
+            System.out.println("Is Palindrom? :"+ isPalindrome(input));
 
             System.out.println("Is Palindrom? :"+ palidrome(input,0,input.length()-1));
 
             sc.close();
+            scanner.close();
         }
+    }
