96b0e5d9dd0dc066a220aa976e9034f7555e1956
bhoomip16 <bhoomistudy16@gmail.com>
2026-03-02 23:41:38 +0530

    UC8: Recursive Palindrome Checker

--- src/Main.java
+++ src/Main.java
@@ -68,30 +68,21 @@ public class PalindromeCheckerApp {
             current = nextNode;
         }
         return prev;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Input text : ");
-        String input = sc.nextLine();
-        Deque<Character> dq = new ArrayDeque<>();
-        boolean palindrome = true;
-
-        for(char c : input.toCharArray()){
-            dq.addLast(c);
+        static boolean palidrome(String s , int start , int end){
+            if(start >= end) return  true;
+            if(s.charAt(start)!=s.charAt(end)) return false;
+            return palidrome(s,start+1,end -1);
         }
-        while(dq.size() > 1){
-            char first = dq.removeFirst();
-            char second = dq.removeLast();
-            if(first != second) palindrome = false;
-            break;
-        }
-        System.out.println("Is Palindrome? :" + palindrome);
-    }
-}
-Node head = createList(input);
-        System.out.println("Is Palindrom? :"+isPalindrome(head));
 
-        sc.close();
-    }
-            }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Input text : ");
+            String input = sc.nextLine();
+
+            Node head = createList(input);
+            System.out.println("Is Palindrom? :"+isPalindrome(head));
+
+            System.out.println("Is Palindrom? :"+ palidrome(input,0,input.length()-1));
+
+            sc.close();
+        }
